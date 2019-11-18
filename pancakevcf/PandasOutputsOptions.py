074f0c14ae5934cab32f4830d2fa56d480363379
arontommi <arontommi@gmail.com>
2019-11-18 14:43:17 +0100

    adding a base option foru output

--- pancakevcf/PandasOutputsOptions.py
+++ pancakevcf/PandasOutputsOptions.py
@@ -1,1 +1,9 @@
 #TODO store what pandas columns to return
+from pancakevcf.HeaderExtraction import HeaderExtract
+
+
+class BaseProfile:
+    """
+    only the most simple view of the pandas df
+    """
+    base_columns = ['#CHROM','POS','REF','ALT']
