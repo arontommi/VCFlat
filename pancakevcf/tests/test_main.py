010acd019b654b166f318ce75b007e1426a072cc
arontommi <arontommi@gmail.com>
2019-11-18 14:44:15 +0100

    some testing bonanza

--- pancakevcf/tests/test_main.py
+++ pancakevcf/tests/test_main.py
@@ -5,13 +5,30 @@ from pancakevcf.VcfParse import *
 
 class TestVcfParse(unittest.TestCase):
 
+    def setUp(self):
+        self.HERE = os.path.dirname(__file__)
+        self.VCF_PATH = os.path.join(self.HERE, "test.vcf.gz")
+        self.vp = VcfParse(self.VCF_PATH)
+        self.df = self.vp.main_parse(input_vcf=self.VCF_PATH)
+
     def test_flatten(self):
         """
         test if final pandas dataframe is returned and in the correct shape
         """
-        HERE = os.path.dirname(__file__)
-        VCF_PATH = os.path.join(HERE, "test.vcf.gz")
-        vp = VcfParse(VCF_PATH)
-        df = vp.main_parse(input_vcf=VCF_PATH)
-        self.assertEqual(str(type(df)), "<class 'pandas.core.frame.DataFrame'>")
-        self.assertEqual(df.shape, (115, 1587))
+        self.assertEqual(str(type(self.df)), "<class 'pandas.core.frame.DataFrame'>")
+        self.assertEqual(self.df.shape, (115, 1587))
+
+    def test_base(self):
+        """
+        Test if the first 9 columns are what they are supposed to be
+        :return:
+        """
+        self.assertEqual(self.vp.vcf_header[:9],['#CHROM',
+                                            'POS',
+                                            'ID',
+                                            'REF',
+                                            'ALT',
+                                            'QUAL',
+                                            'FILTER',
+                                            'INFO',
+                                            'FORMAT'])
