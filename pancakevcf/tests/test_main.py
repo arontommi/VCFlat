e67fad778277c0c4c6a3a320077875250d2ca725
arontommi <arontommi@gmail.com>
2019-11-19 13:55:30 +0100

    tests fixed based on new vcfparse

--- pancakevcf/tests/test_main.py
+++ pancakevcf/tests/test_main.py
@@ -8,27 +8,29 @@ class TestVcfParse(unittest.TestCase):
     def setUp(self):
         self.HERE = os.path.dirname(__file__)
         self.VCF_PATH = os.path.join(self.HERE, "test.vcf.gz")
-        self.vp = VcfParse(self.VCF_PATH)
-        self.df = self.vp.main_parse(input_vcf=self.VCF_PATH)
+        self.vp_1 = VcfParse(self.VCF_PATH)
+        self.VCF_PATH = os.path.join(self.HERE, "issue_44.vcf")
+        self.vp_2 = VcfParse(self.VCF_PATH)
+
 
     def test_flatten(self):
         """
         test if final pandas dataframe is returned and in the correct shape
         """
-        self.assertEqual(str(type(self.df)), "<class 'pandas.core.frame.DataFrame'>")
-        self.assertEqual(self.df.shape, (115, 1587))
+        self.assertEqual(str(type(self.vp_1.df)), "<class 'pandas.core.frame.DataFrame'>")
+        self.assertEqual(self.vp_1.df.shape, (115, 1587))
 
     def test_base(self):
         """
         Test if the first 9 columns are what they are supposed to be
         :return:
         """
-        self.assertEqual(self.vp.vcf_header[:9],['#CHROM',
+        self.assertEqual(self.vp_1.vcf_header[:8],['#CHROM',
                                             'POS',
                                             'ID',
                                             'REF',
                                             'ALT',
                                             'QUAL',
                                             'FILTER',
-                                            'INFO',
-                                            'FORMAT'])
+                                            'INFO'])
+
