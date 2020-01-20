17e099f0dbf631112caad12e2a0290f3047d0356
arontommi <arontommi@gmail.com>
2020-01-20 14:51:46 +0100

    test for clean meta func

---
+++ vcflat/tests/test_HeaderExtraction/test_clean_meta.py
@@ -0,0 +1,28 @@
+import os
+from vcflat.HeaderExtraction import  get_raw_header, pop_header, clean_meta
+
+def get_input():
+    test_data_dir = os.path.join(os.path.dirname(__file__), '..', 'test_data')
+    i = os.path.join(test_data_dir, "test.snpeff.vcf")
+    return i
+
+def base_tests():
+    i = get_input()
+    output = get_raw_header(i)
+    clean = clean_meta(output)
+    return clean
+
+def test_1():
+    """ checks that output is list """
+    assert type(base_tests()) is list
+
+def test_2():
+    """ checks that output is not empty """
+    assert len(base_tests()) != 0
+
+def test_3():
+    """ checks meta has been cleaned"""
+    for i in ['=<', '##']:
+        assert i not in base_tests()
+
+
