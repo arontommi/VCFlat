3d7ba9b944a267b3b14dfb62ceb66dd29335c823
arontommi <arontommi@gmail.com>
2020-01-20 14:52:22 +0100

    tests for dictify

---
+++ vcflat/tests/test_HeaderExtraction/test_dictify.py
@@ -0,0 +1,25 @@
+import os
+from vcflat.HeaderExtraction import  get_raw_header , dictify, clean_meta
+from collections import defaultdict
+def get_input():
+    test_data_dir = os.path.join(os.path.dirname(__file__), '..', 'test_data')
+    i = os.path.join(test_data_dir, "test.snpeff.vcf")
+    return i
+
+def base_tests():
+    i = get_input()
+    output = get_raw_header(i)
+    clean = clean_meta(output)
+    dictified = dictify(clean)
+    return dictified
+
+def test_1():
+    """ checks that output is list """
+    assert type(base_tests()) is type(defaultdict())
+
+def test_2():
+    """ checks that output is not empty """
+    assert len(base_tests()) != 0
+
+
+
