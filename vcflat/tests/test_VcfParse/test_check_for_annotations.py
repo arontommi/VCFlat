770f4447c0c230ff9985819149974952c6c44f9c
arontommi <arontommi@gmail.com>
2020-06-09 11:36:48 +0200

    test added

--- vcflat/tests/test_VcfParse/test_check_for_annotations.py
+++ vcflat/tests/test_VcfParse/test_check_for_annotations.py
@@ -1,17 +1,18 @@
 import os
 import vcflat.VcfParse as VP
 
+
 def get_input():
     test_data_dir = os.path.join(os.path.dirname(__file__), '..', 'test_data')
     i = os.path.join(test_data_dir, "test.snpeff.vcf")
     return i
+
 
 def base_tests():
     i = get_input()
     vcffile = VP.VcfParse(i,'Name')
     return vcffile
 
-def test_1():
-    """ test if csq flag ist as expected"""
-    assert base_tests().csq_flag() is True
 
+def test_1():
+    assert base_tests().anno_field == 'EFF'
